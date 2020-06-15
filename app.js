aefcc99dd014bb0428e310e820eae2c19e636770
Wahab Ahmed <wahab.wajahat.ahmed@ibm.com>
2020-06-15 00:56:12 -0400

    Added routing for user info.

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var config = require('./config/config');
-const fileUpload = require('express-fileupload');
 
 
 // routes
@@ -22,8 +21,6 @@ const app = express();
 
 // CORS Middleware
 app.use(cors());
-app.use(fileUpload());
-console.log("HERRE")
 // Logger Middleware
 app.use(morgan('dev'));
 // Bodyparser Middleware
@@ -39,13 +36,17 @@ mongoose
     useCreateIndex: true,
     useUnifiedTopology: true
   }) // Adding new mongo url parser
-  .then(() => console.log('MongoDB ...'))
+  .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
 // Use Routes
+// app.use(createProxyMiddleware(['/api' ], { target: process.env.PORT || 'http://localhost:3001' }));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
-// app.use('/api/files', fileRoutes);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
