222261fa13cc6f13eab5e47e89fa4ba8d1b0336d
Wahab Ahmed <wahab.wajahat.ahmed@ibm.com>
2020-06-14 20:14:46 -0400

    A lot of updates..

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var config = require('./config/config');
+const fileUpload = require('express-fileupload');
 
 
 // routes
 var authRoutes = require('./routes/auth');
 var userRoutes = require('./routes/users');
+var fileRoutes = require('./routes/files')
 
 const { MONGO_URI, MONGO_DB_NAME } = config;
 console.log("CONFIG", config);
@@ -21,6 +23,8 @@ const app = express();
 
 // CORS Middleware
 app.use(cors());
+app.use(fileUpload());
+console.log("HERRE")
 // Logger Middleware
 app.use(morgan('dev'));
 // Bodyparser Middleware
@@ -40,13 +44,9 @@ mongoose
   .catch(err => console.log(err));
 
 // Use Routes
-// app.use(createProxyMiddleware(['/api' ], { target: process.env.PORT || 'http://localhost:3001' }));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/files', fileRoutes);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
